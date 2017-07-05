f5f891d5b851599912f608af76ae1f9054c1c35f
seront <seront.nmmc@gmail.com>
2017-07-05 08:39:30 -0400

    cambiando la forma en que declaro el modulo access

--- src/access/index.js
+++ src/access/index.js
@@ -7,8 +7,15 @@ import {AccessMain} from './accessMain.js';
 import {AccessLogin} from './accessLogin.js';
 import {AccessRecuperar} from './accessRecuperar.js';
 
-export default angular.module('app.access', [uirouter])
+// export default angular.module('app.access', [uirouter])
+//   .component('accessMain', AccessMain)
+//   .component('accessLogin', AccessLogin)
+//   .component('accessRecuperar', AccessRecuperar)
+//   .config(routing).name;
+let AccessModule = angular.module('app.access', [uirouter])
   .component('accessMain', AccessMain)
   .component('accessLogin', AccessLogin)
   .component('accessRecuperar', AccessRecuperar)
   .config(routing).name;
+
+export {AccessModule};

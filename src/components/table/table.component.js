d667a1fed31e11468a721464567ce4c940af28b4
seront <seront.nmmc@gmail.com>
2017-07-28 12:48:54 -0400

    quitando un console

--- src/components/table/table.component.js
+++ src/components/table/table.component.js
@@ -1,51 +1,45 @@
 class TableController {
-  constructor($log) {
+  constructor() {
     'ngInject';
-    //  console.log("AccessMainController componente");
-    this.$log = $log;
   }
   $onInit() {
     this.newConfig = [];
     this.order();
     this.selected = [];
-    if(this.pagination){
+    if (this.pagination) {
       this.prevLimit = this.pagination.limit;
-    this.prevPage = this.pagination.page;
+      this.prevPage = this.pagination.page;
     }
   }
 
   order() {
     var salida = [];
     for (var item in this.objectConfig) {
-        var ob = [item, this.objectConfig[item]];
-        salida.push(ob);
+      var ob = [item, this.objectConfig[item]];
+      salida.push(ob);
     }
     salida.sort(function (a, b) {
       return (parseInt(a[1].order, 10) > parseInt(b[1].order, 10) ? 1 : -1);
     });
     this.newConfig = salida;
-    // this.$log.log(salida);
   }
 
-  paginar(){
-    if(this.prevLimit !== this.pagination.limit || this.prevPage !== this.pagination.page){
-       console.log("paginar ", this.pagination.page, this.pagination.limit);
-        this.prevLimit = this.pagination.limit;
-    this.prevPage = this.pagination.page;
-    this.onPaginate({page: this.pagination.page, limit: this.pagination.limit})
+  paginar() {
+    if (this.prevLimit !== this.pagination.limit || this.prevPage !== this.pagination.page) {
+      this.prevLimit = this.pagination.limit;
+      this.prevPage = this.pagination.page;
+      this.onPaginate({ page: this.pagination.page, limit: this.pagination.limit })
     }
   }
 
-  $onChanges(cambios){
-    if(cambios.pagination && cambios.pagination.currentValue){
+  $onChanges(cambios) {
+    if (cambios.pagination && cambios.pagination.currentValue) {
       this.pagination = cambios.pagination.currentValue;
     }
 
-    if(cambios.actions && cambios.actions.currentValue){
+    if (cambios.actions && cambios.actions.currentValue) {
       this.actions = angular.copy(cambios.actions.currentValue);
     }
-
-
   }
 }
 

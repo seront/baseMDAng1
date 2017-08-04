47b498a4f541e99187128ea1878b0b9855a18945
seront <seront.nmmc@gmail.com>
2017-08-04 11:48:04 -0400

    moviendo el modulo access completo a otro repositorio

--- src/components/components.module.js
+++ src/components/components.module.js
@@ -1,14 +1,9 @@
 import angular from 'angular';
 
-import {default as Access} from './access/access.module';
 import {HeaderComponent} from './header/header.component'
 import {SidemenuComponent} from './sidemenu/sidemenu.component'
 
-export default angular.module('medipass.components', [
-  Access
-  // ,
-  // Sidemenu
-  ])
+export default angular.module('medipass.components', [])
   .component('header', HeaderComponent)
   .component('sidemenu', SidemenuComponent)
   .name;
